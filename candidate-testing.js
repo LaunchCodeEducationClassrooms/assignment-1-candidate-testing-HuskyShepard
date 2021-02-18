8b97774778dc91c23a2c03bfcd26a785da07f395
HuskyShepard <josephwramseyer@gmail.com>
2021-02-18 23:53:37 +0000

    Made test

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,58 +1,36 @@
 const input = require('readline-sync');
-
-// TODO 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
-let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
-
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
-}
-
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
-}
-
-function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
+// define questions
+module.exports.runProgram = function() {
+  let candidateName = input.question ('Candidate Name: ')
+  let questions = ["Who was the first American woman in space? ",
+   "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ",
+   "Given the array [8, Orbit, Trajectory, 45] what entry is at index 2? ",
+   "What is the minimum crew size for the International Space Station(ISS)? "];
+  let answers = ["sally ride","true","40","trajectory","3"]
+  let response = [];
+  let correctAnswers = 5;
   
+  // ask each question
 
-  return grade;
-}
-
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
   
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-}
+    // calculate score
+  let numberCorrect = 0;
+  let score;
 
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
+
+  for(let i = 0; i < questions.length; i++){
+    console.log(`${i+1}) ${questions[i]}`);
+    
+    response[i] = input.question(`Your Answer: `);
+    console.log(`Correct Answer: ${answers[i]}`);
+    if(response[i].toLowerCase() === answers[i].toLowerCase()){
+      numberCorrect++;
+    }
+    console.log("");
+  }
+
+  score = (numberCorrect/questions.length) * 100;
+
+  console.log(`>>> Overall Grade: ${score}% (${numberCorrect} of ${questions.length} responses correct) <<<`);
+  console.log(`>>> Status: ${(score >= 80)?'PASSED':'FAILED'} <<<`)
 };
