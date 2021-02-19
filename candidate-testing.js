1a00b4a8d44316a470e547c218898cdec1186ce1
HuskyShepard <josephwramseyer@gmail.com>
2021-02-19 00:14:31 +0000

    formatting

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,11 +1,15 @@
 const input = require('readline-sync');
-// define questions
+
+// TODO 2: modify your quiz app to ask 5 questions //
+
+// TODO 1.1a: Define candidateName // 
 module.exports.runProgram = function() {
-  let candidateName = input.question ('Candidate Name: ')
-  let questions = ["Who was the first American woman in space? ",
-   "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ",
-   "Given the array [8, Orbit, Trajectory, 45] what entry is at index 2? ",
-   "What is the minimum crew size for the International Space Station(ISS)? "];
+let candidateName = input.question ('Candidate Name: ')
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let questions = ["Who was the first American woman in space? ",
+"True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ",
+"Given the array [8, Orbit, Trajectory, 45] what entry is at index 2? ",
+"What is the minimum crew size for the International Space Station(ISS)? "];
   let answers = ["sally ride","true","40","trajectory","3"]
   let response = [];
   let correctAnswers = 5;
@@ -33,4 +37,4 @@ module.exports.runProgram = function() {
 
   console.log(`>>> Overall Grade: ${score}% (${numberCorrect} of ${questions.length} responses correct) <<<`);
   console.log(`>>> Status: ${(score >= 80)?'PASSED':'FAILED'} <<<`)
-};
+;}
